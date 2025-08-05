930610503743e525fe0fe431a57cad0566c0f0c7
Martin Androvich <martinandrovich@gmail.com>
2025-08-05 15:25:52 +0200

    Refactor dietist_scan_medcom_letters.py

--- src/nyborg_rpa/scripts/nexus/dietist_scan_medcom_letters.py
+++ src/nyborg_rpa/scripts/nexus/dietist_scan_medcom_letters.py
@@ -7,10 +7,15 @@ from dotenv import load_dotenv
 from office365.graph_client import GraphClient
 from tqdm.auto import tqdm
 
+from nyborg_rpa.utils.auth import get_user_login_info
 from nyborg_rpa.utils.nexus_client import NexusClient
 from nyborg_rpa.utils.send_mail import send_email
 
-EXPECTED_DISTRICT = [
+sharepoint_client: GraphClient
+nexus_environment: str
+nexus_client: NexusClient
+
+EXPECTED_DISTRICTS: tuple[dict] = (
     {"name": "Distrikt Aften By", "active": False},
     {"name": "Distrikt Aften Land", "active": False},
     {"name": "Distrikt Egepark", "active": True},
@@ -21,19 +26,16 @@ EXPECTED_DISTRICT = [
     {"name": "Distrikt Svanedam Øst", "active": True},
     {"name": "E-distrikt.", "active": False},
     {"name": "Private leverandører", "active": False},
-]
-
-# nexus_client = NexusClient(enviroment="nexus-review")
-enviroment = "nexus"
-nexus_client = NexusClient(enviroment=enviroment)
+)
 
 
-def get_medcom_letters(activity_liste: str) -> list:
+def fetch_medcom_letters(activity_name: str) -> list[dict]:
+
     resp = nexus_client.get("preferences/ACTIVITY_LIST/")
     activity_list = resp.json()
 
     for item in activity_list:
-        if item["name"] == activity_liste:
+        if item["name"] == activity_name:
             activity_link = item["_links"]["self"]["href"]
             break
 
@@ -41,45 +43,41 @@ def get_medcom_letters(activity_liste: str) -> list:
     discharge_report = resp.json()
 
     content_link = discharge_report["_links"]["content"]["href"]
-
-    # to_date = pd.Timestamp.now() - pd.Timedelta(days=180)
     to_date = pd.Timestamp.now()
-    to_date_str = to_date.strftime("%Y-%m-%dT%H:%M:%S.999Z")
     from_date = to_date - pd.Timedelta(days=7)
-    from_date_str = from_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
-    resp = nexus_client.get(f"{content_link}&pageSize=50&from={from_date_str}&to={to_date_str}")
+    resp = nexus_client.get(f"{content_link}&pageSize=50&from={from_date:%Y-%m-%dT%H:%M:%S}.000Z&to={to_date:%Y-%m-%dT%H:%M:%S}.999Z")
     content_links = resp.json()
 
-    letter_list = []
-
-    # print("Loading pages")
-    # for page in tqdm(content_links["pages"]):
+    letters = []
     for page in content_links["pages"]:
+
         page_content = nexus_client.get(page["_links"]["content"]["href"]).json()
         for content in page_content:
-            # date = datetime.strptime(content["date"], '%Y-%m-%dT%H:%M:%S.%f%z')
             date = pd.to_datetime(content["date"], format="%Y-%m-%dT%H:%M:%S.%f%z", errors="coerce")
             medcom_id = content["_links"]["referencedObject"]["href"].split("/")[-1]
-            letter = {
-                "medcom_id": medcom_id,
-                "name": content["name"],
-                "patients_id": content["patients"][0]["id"],
-                "date": date,
-                "link": content["_links"]["referencedObject"]["href"],
-            }
-            letter_list.append(letter)
-    return letter_list
+            letters += [
+                {
+                    "medcom_id": medcom_id,
+                    "name": content["name"],
+                    "patients_id": content["patients"][0]["id"],
+                    "date": date,
+                    "link": content["_links"]["referencedObject"]["href"],
+                }
+            ]
+
+    return letters
 
 
-def get_ogranisation_tree_info(organisation: str) -> dict:
+def get_organization_tree_info(name: str) -> dict:
+
     resp = nexus_client.get("organizations/tree?activeOnly=false")
     data = resp.json()
-    ids = []
+    ids = set()
 
     def create_sub_tree_id_list(item: list):
         for row in item:
-            ids.append(row["id"])
+            ids.add(row["id"])
             create_sub_tree_id_list(row["children"])
 
     def find_sub_tree(*, item: dict, name: str):
@@ -92,14 +90,21 @@ def get_ogranisation_tree_info(organisation: str) -> dict:
                 if tree is not None:
                     return tree
 
-    result = {
-        "distric_tree": find_sub_tree(item=data, name=organisation),
-        "distric_ids": set(ids),
-    }
-    return result
+    districts = find_sub_tree(item=data, name=name)
+
+    if mismatched_districts := ({d["name"] for d in districts} ^ {d["name"] for d in EXPECTED_DISTRICTS}):
+        raise ValueError(f"Found mismatched districts: {mismatched_districts}.")
+
+    return {"distric_tree": districts, "district_ids": ids}
 
 
-def find_active_organisation(patient: str, distric_ids: set, distric_tree: list) -> str:
+def find_active_organisation(
+    *,
+    patient: str,
+    district_ids: set,
+    distric_tree: list,
+) -> str:
+
     resp = nexus_client.get(f"patients/{patient}/organizations")
     data = resp.json()
 
@@ -114,38 +119,40 @@ def find_active_organisation(patient: str, distric_ids: set, distric_tree: list)
                 if found_id:
                     return found_id
 
-    found_distrikt = set()
+    found_district = set()
     found_distrikt_id = []
     for org in data:
-        if org["id"] in distric_ids and org["effectiveAtPresent"]:
+        if org["id"] in district_ids and org["effectiveAtPresent"]:
             found_distrikt_id.append(org["id"])
             for item in distric_tree:
                 check_id = find_parent_by_id(item, org["id"])
                 if check_id:
-                    found_distrikt.add(item["name"])
+                    found_district.add(item["name"])
                     break
 
-    active_distrik = []
+    active_districts = []
 
-    for item in found_distrikt:
-        for district in EXPECTED_DISTRICT:
+    for item in found_district:
+        for district in EXPECTED_DISTRICTS:
             if item == district["name"] and district["active"]:
-                active_distrik.append(item)
+                active_districts.append(item)
 
-    if len(active_distrik) == 1:
-        found_distrikt = active_distrik[0]
+    if len(active_districts) == 1:
+        found_district = active_districts[0]
     else:
-        found_distrikt = "Ukendt"
+        found_district = "Ukendt"
 
-    return found_distrikt
+    return found_district
 
 
-def create_mail_message(mails: dict) -> str:
-    brødtekst = """<!DOCTYPE html>
+def generate_report_email(letters: list[dict]) -> str:
+
+    typography_style = "font-family: Arial, sans-serif; font-size: 12px"
+    body = f"""<!DOCTYPE html>
     <html>
-    <body style="font-family: Arial, sans-serif; font-size: 14px; margin: 0; padding: 0;">
-    <h2 style="font-size: 13px; font-weight: bold;">Robotten har netop scannet nye breve og identificeret relevante ord i følgende dokumenter:</h2>
-    <table border="1" cellpadding="4" cellspacing="0" width="100%" style="border-collapse: collapse; font-size: 14px;">
+    <body style="margin: 0; padding: 0; {typography_style};">
+    <p>Robotten har netop scannet nye breve og identificeret relevante ord i følgende dokumenter:</p>
+    <table border="1" cellpadding="4" cellspacing="0" width="100%" style="border-collapse: collapse; {typography_style};">
         <tr style="background-color: #dddddd; font-weight: bold;">
         <td>Emne</td>
         <td>Dato</td>
@@ -154,93 +161,56 @@ def create_mail_message(mails: dict) -> str:
         <td>Nøgleord</td>
         </tr>"""
 
-    # Group and sort by district
-    grouped = defaultdict(list)
-    for mail in mails:
-        if mail["found_words"]:
-            grouped[mail["district"]] += [mail]
+    # group and sort by district
+    letters_by_district = defaultdict(list)
+    for letter in letters:
+        if letter["keywords"]:
+            letters_by_district[letter["district"]] += [letter]
 
-    for district in sorted(grouped.keys()):
-        brødtekst += f"""
+    for district in sorted(letters_by_district.keys()):
+
+        body += f"""
         <tr style="background-color: #f0f0f0; font-weight: bold;">
         <td colspan="5">{district}</td>
         </tr>"""
-        for mail in grouped[district]:
-            nøgleord = ", ".join(mail["found_words"])
-            brødtekst += f"""
-        <tr>
-        <td>{mail['letter']['name']}</td>
-        <td>{mail['letter']['date'].strftime("%Y-%m-%d %H:%M:%S")}</td>
-        <td>{mail['letter']['patients_id']}</td>
-        <td><a href="https://nyborg.{enviroment}.kmd.dk/citizen/{mail['letter']['patients_id']}/correspondence/inbox" style="color: #0000EE;">Åbn indbakke</a></td>
-        <td>{nøgleord}</td>
-        </tr>"""
 
-    brødtekst += """
+        for letter in letters_by_district[district]:
+            keywords = ", ".join(letter["keywords"])
+            body += f"""
+            <tr>
+            <td>{letter["name"]}</td>
+            <td>{letter["date"].strftime("%Y-%m-%d %H:%M:%S")}</td>
+            <td>{letter["patients_id"]}</td>
+            <td><a href="https://nyborg.{nexus_environment}.kmd.dk/citizen/{letter["patients_id"]}/correspondence/inbox" style="color: #0000EE;">Åbn indbakke</a></td>
+            <td>{keywords}</td>
+            </tr>"""
+
+    body += """
     </table>
-    <p>Venlig hilsen,
-
-    Robotten</p>
-    </body>
-    </html>"""
-    return brødtekst
-
-
-def create_mail_message_1(mails: dict) -> str:
-    brødtekst = """<!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <style>
-        body { font-family: Arial, sans-serif; }
-        .container { max-width: 600px; margin: 0 auto; padding: 1em; background-color: #ffffff; border-radius: 8px; }
-        .document { margin-bottom: 20px; padding: 1em; background-color: #fff; border: 1px solid #ddd; border-radius: 6px; }
-        .document h3 { margin-top: 0; }
-        .found-words { margin-top: 8px; padding-left: 20px; }
-    </style>
-    </head>
-    <body>
-    <div class="container">
-        <h2>Resultat af den daglige scanning: Fund af Ernæringsord</h2>
-        <p>Hej,</p>
-        <p>Robotten har netop scannet nye breve og fundet følgende relevante ord i nedenstående dokumenter:</p>"""
-
-    for mail in mails:
-        if mail["found_words"] == []:
-            continue
-        tekst = f"""
-        <div class="document">
-        <p><strong>Distrikt: </strong> {mail['district']}<br>
-            <strong>Emne: </strong> {mail['letter']['name']}<br>
-            <strong>Dato:</strong> {mail['letter']['date'].strftime("%Y-%m-%d %H:%M:%S")}<br>
-            <strong>Patients ID:</strong> {mail['letter']['patients_id']}<br>
-            <strong>Link til patient indbakke:</strong> <a href="https://nyborg.{enviroment}.kmd.dk/citizen/{mail['letter']['patients_id']}/correspondence/inbox">Åbn indbakke</a></p>
-        <p><strong>Fundne ord:</strong> {mail['found_words']}</p>
-        </div>
-        """
-
-        brødtekst += tekst
-
-    brødtekst += """<p>Venlig hilsen,<br>
-        Robotten</p>
-    </div>
+    <p>Venlig hilsen,<br>Robotten</p>
     </body>
     </html>"""
 
-    return brødtekst
+    return body
 
 
-if __name__ == "__main__":
-    load_dotenv(override=True)
+def scan_medcom_letters():
 
-    sharepoint_client = GraphClient(tenant=os.getenv("MS_GRAPH_TENANT_ID")).with_client_secret(
-        client_id=os.getenv("MS_GRAPH_CLIENT_ID"),
-        client_secret=os.getenv("MS_GRAPH_CLIENT_SECRET"),
+    # get organization tree info
+    org_info = get_organization_tree_info(name="Hjemmepleje")
+
+    # fetch SharePoint lists
+    print("Fetching SharePoint lists...")
+
+    sp_keywords_list = (
+        sharepoint_client.sites.get_by_url(url="https://nyborg365.sharepoint.com/sites/RPADrift")
+        .lists.get_by_name(name="01.53.02 Ernæringsord liste")
+        .items.get_all()
+        .expand(["fields"])
+        .execute_query()
     )
 
-    items = sharepoint_client.sites.get_by_url("https://nyborg365.sharepoint.com/sites/RPADrift").lists.get_by_name("01.53.02 Ernæringsord liste").items.get_all().expand(["fields"]).execute_query()
-
-    sp_list = (
+    sp_prev_letters_list = (
         sharepoint_client.sites.get_by_url("https://nyborg365.sharepoint.com/sites/RPADrift")
         .lists.get_by_name("01.53.01  Sundhed og Ældre - Diætist - Medcom scanner")
         .items.get_all()
@@ -248,60 +218,90 @@ if __name__ == "__main__":
         .execute_query()
     )
 
-    def check_letters(letter_list: dict):
-        for letter in tqdm(letter_list):
-            found_words = []
-            link = letter["link"]
-            if letter["medcom_id"] in former_runs_item:
-                continue
-            medcom = nexus_client.get(link).json()
-            decoded_xml = base64.b64decode(medcom["raw"]).decode("utf-8")
-            for i, item in enumerate(items):
-                search_word = item.properties["fields"].properties["Title"]
-                current_udslag = item.properties["fields"].properties["Udslag"]
-                if search_word.lower() in decoded_xml.lower():
-                    found_words.append(search_word.lower())
-                    item.fields.set_property("Udslag", current_udslag + 1)
-                    item.fields.update()
-            # if found_words != []:
-            mail = {
-                "letter": letter,
-                "found_words": found_words,
-                "district": find_active_organisation(patient=letter["patients_id"], distric_ids=organiastion["distric_ids"], distric_tree=organiastion["distric_tree"]),
-            }
-            mails.append(mail)
-            # Testing
-            fields = {
-                "Title": mail["letter"]["medcom_id"],
-                "Aktivitetsliste": mail["letter"]["name"],
-                "Match": True if mail["found_words"] != [] else False,
+    # fetch letters to check
+    medcom_activities = ("Udskrivningsrapport", "Plejeforløbsplaner")
+    prev_letters = {(item.properties["fields"].properties["Title"], item.properties["fields"].properties["Aktivitetsliste"]) for item in sp_prev_letters_list}
+    letters = [letter for activity in medcom_activities for letter in fetch_medcom_letters(activity)]
+
+    letters_to_report = []
+    for letter in tqdm(letters, desc="Processing letters"):
+
+        # skip if letter already processed
+        if (letter["medcom_id"], letter["name"]) in prev_letters:
+            print(f"Skipping {letter=}")
+            continue
+
+        # fetch letter body
+        letter_body_raw = nexus_client.get(letter["link"]).json()
+        letter_body = base64.b64decode(letter_body_raw["raw"]).decode("utf-8")
+
+        # search for keywords in letter body based on SharePoint items
+        keywords = []
+        for item in sp_keywords_list:
+            search_word = str(item.properties["fields"].properties["Title"]).lower()
+            num_matches = int(item.properties["fields"].properties["Udslag"])
+            if search_word in letter_body.lower():
+                keywords += [search_word]
+                item.fields.set_property("Udslag", num_matches + 1)
+                item.fields.update()  # needs .execute_query() to take effect
+
+        # save to list of processed letters
+        sp_prev_letters_list.add(
+            fields={
+                "Title": letter["medcom_id"],
+                "Aktivitetsliste": letter["name"],
+                "Match": bool(keywords),
                 "Status": "Completed",
-                "Dato": mail["letter"]["date"].strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "Dato": letter["date"].strftime("%Y-%m-%dT%H:%M:%SZ"),
             }
-            sp_list.add(fields=fields)
+        )
 
-    Udskrivningsrapport = get_medcom_letters("Udskrivningsrapport")
-    Plejeforløbsplaner = get_medcom_letters("Plejeforløbsplaner")
-    organiastion = get_ogranisation_tree_info("Hjemmepleje")
-    # active_organisation = set(find_active_organisation(patient=1, distric_ids=organiastion["distric_ids"], distric_tree=organiastion["distric_tree"]))
+        # if letter contains keywords, add to list of matching letters
+        # which will be used to generate the report email
+        if keywords:
+            district = find_active_organisation(patient=letter["patients_id"], district_ids=org_info["district_ids"], distric_tree=org_info["distric_tree"])
+            letter |= {"keywords": keywords, "district": district}
+            letters_to_report += [letter]
 
-    main_district = {district["name"] for district in organiastion["distric_tree"]}
-    expected_dist = {district["name"] for district in EXPECTED_DISTRICT}
-    if expected_dist != main_district:
-        raise ValueError("Distrikterne har ændret sig")
+    # send email if there are letters to report
+    if letters_to_report:
 
-    mails = []
-    former_runs_item = [item.properties["fields"].properties["Title"] for item in sp_list if item.properties["fields"].properties["Aktivitetsliste"] == "Udskrivningsrapport"]
-    print("checking letters in Udskrivningsrapport")
-    check_letters(Udskrivningsrapport)
-    former_runs_item = [item.properties["fields"].properties["Title"] for item in sp_list if item.properties["fields"].properties["Aktivitetsliste"] == "Plejeforløbsplan"]
-    print("checking letters in Plejeforløbsplaner")
-    check_letters(Plejeforløbsplaner)
+        print("Sending report email...")
+        send_email(
+            from_addr=os.environ["MS_MAILBOX"],
+            to_addr="emia@nyborg.dk",
+            subject="Rapport: Fund af ernæringsrelaterede ord",
+            body=generate_report_email(letters_to_report),
+        )
 
-    if len(mails) > 0:
-        print("Sender brev...")
-        from_addr = os.getenv("MS_MAILBOX")
-        brødtekst = create_mail_message(mails)
-        send_email(from_addr=from_addr, to_addr="emia@nyborg.dk", subject="Rapport: Fund af ernæringsrelaterede ord", body=brødtekst)
-        print("loading to sharepoint...")
-        sharepoint_client.execute_query()
+    # save changes to SharePoint
+    # print("Saving changes to SharePoint...")
+    # sharepoint_client.execute_query()
+
+
+if __name__ == "__main__":
+
+    # initialize clients
+
+    load_dotenv(override=True)
+
+    login_info = get_user_login_info(
+        username="API",
+        program="Nexus-Drift",
+    )
+
+    nexus_environment = "nexus"
+    nexus_client = NexusClient(
+        client_id=login_info["username"],
+        client_secret=login_info["password"],
+        instance="nyborg",
+        enviroment=nexus_environment,
+    )
+
+    sharepoint_client = GraphClient(tenant=os.getenv("MS_GRAPH_TENANT_ID")).with_client_secret(
+        client_id=os.getenv("MS_GRAPH_CLIENT_ID"),
+        client_secret=os.getenv("MS_GRAPH_CLIENT_SECRET"),
+    )
+
+    # scan Medcom letters for keywords
+    scan_medcom_letters()
