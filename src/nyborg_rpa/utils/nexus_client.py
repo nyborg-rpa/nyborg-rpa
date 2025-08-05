e2d90fbacc96bfbe8a756cd41b6544699d6a798b
Martin Androvich <martinandrovich@gmail.com>
2025-08-05 15:18:31 +0200

    Add client_id etc. args to NexusClient

--- src/nyborg_rpa/utils/nexus_client.py
+++ src/nyborg_rpa/utils/nexus_client.py
@@ -2,20 +2,30 @@ from typing import Literal
 
 from authlib.integrations.httpx_client import OAuth2Client
 
-from nyborg_rpa.utils.auth import get_user_login_info
-
 
 class NexusClient(OAuth2Client):
 
-    def __init__(self, enviroment: Literal["nexus", "nexus-review"]):
+    def __init__(
+        self,
+        *,
+        client_id: str,
+        client_secret: str,
+        instance: str,
+        enviroment: Literal["nexus", "nexus-review"],
+    ):
+        """
+        Initialize the NexusClient with OAuth2 credentials and base URL.
 
-        instance = "nyborg"
-        program = {"nexus": "Nexus-Drift", "nexus-review": "Nexus-Test"}[enviroment]
-        nexus_info = get_user_login_info(username="API", program=program)
+        Args:
+            client_id: The client ID for OAuth2 authentication.
+            client_secret: The client secret for OAuth2 authentication.
+            instance: The `https://{instance}.{enviroment}.kmd.dk` instance.
+            enviroment: The `https://{instance}.{enviroment}.kmd.dk` environment.
+        """
 
         super().__init__(
-            client_id=nexus_info["username"],
-            client_secret=nexus_info["password"],
+            client_id=client_id,
+            client_secret=client_secret,
             token_endpoint=f"https://iam.{enviroment}.kmd.dk/authx/realms/{instance}/protocol/openid-connect/token",
             timeout=30.0,
             base_url=f"https://{instance}.{enviroment}.kmd.dk/api/core/mobile/{instance}/v2/",
@@ -26,5 +36,19 @@ class NexusClient(OAuth2Client):
 
 
 if __name__ == "__main__":
-    nexus_client = NexusClient(enviroment="nexus-review")
+
+    from nyborg_rpa.utils.auth import get_user_login_info
+
+    login_info = get_user_login_info(
+        username="API",
+        program="Nexus-Test",
+    )
+
+    nexus_client = NexusClient(
+        client_id=login_info["username"],
+        client_secret=login_info["password"],
+        instance="nyborg",
+        enviroment="nexus-review",
+    )
+
     print(nexus_client.get("patient/1/preferences/").json())
