50da36a2e3d58d1904be8deb188a04ddc199db28
Emil Damholt Andersen <damholt-a@live.dk>
2025-08-07 08:10:01 +0200

    Add find_employee_email.py

---
+++ src/nyborg_rpa/scripts/sd/find_employee_email.py
@@ -0,0 +1,28 @@
+from nyborg_rpa.utils.os2sofd_client import OS2sofdClient
+from nyborg_rpa.utils.pad import dispatch_pad_script
+
+
+def find_employee_email(*, cpr: str) -> str | None:
+    """
+    Find the email address of an employee based on their CPR number.
+
+    Args:
+        cpr: The CPR number of the employee.
+
+    Returns:
+        str: The email address of the employee if found, otherwise None.
+    """
+    sofd_client = OS2sofdClient(kommune="nyborg")
+    user_info = sofd_client.get_user_by_cpr(cpr=cpr)
+
+    email = next((user["UserId"] for user in user_info["Users"] if "@" in user["UserId"]), None)
+
+    return email
+
+
+if __name__ == "__main__":
+    dispatch_pad_script(fn=find_employee_email)
+
+    # Example usage
+    # email = find_employee_email(cpr="1234567890")
+    # print(email)
