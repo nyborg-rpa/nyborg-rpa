e98ee9218f749750a1a63931d64b15b0a2dbd1a3
Martin Androvich <martinandrovich@gmail.com>
2025-09-19 10:03:26 +0200

    Remove fech_citizens_data() + format DatafordelerClient

--- src/nyborg_rpa/utils/datafordeler.py
+++ src/nyborg_rpa/utils/datafordeler.py
@@ -6,43 +6,9 @@ from pathlib import Path
 from typing import NotRequired, TypedDict
 
 import httpx
-import pandas as pd
 from dotenv import load_dotenv
 
 from nyborg_rpa.utils.cryptography import pfx_cert_to_pem
-
-
-class DatafordelerAddress(TypedDict):
-    vejadresseringsnavn: str
-    husnummer: str
-    postnummer: str
-    postdistrikt: str
-    etage: NotRequired[str]
-    sidedoer: NotRequired[str]
-    bynavn: NotRequired[str]
-
-
-def parse_address(address: DatafordelerAddress) -> str:
-    """Parse a Datafordeler address into a single-line string on the form 'Street Name Nr, Floor Door, Zip City'."""
-
-    # https://danmarksadresser.dk/om-adresser/saadan-gengives-en-adresse
-
-    street_name = address["vejadresseringsnavn"]  # TODO: add supplerende bynavn
-    street_nr = re.sub(r"^0+", "", address["husnummer"])  # 005C, 063, 003A, etc.
-    street = f"{street_name} {street_nr}"
-
-    floor = re.sub(r"^0*(.+)$", r"\1.", address.get("etage", ""))  # "02" -> "2.", "st" -> "st."
-    door = address.get("sidedoer", "")  # "tv", "a19", etc.
-    floor_and_door = " ".join(p for p in (floor, door) if p)
-
-    city = address["postdistrikt"]  # Ebeltoft, Randers C, etc.
-    zip_code = address["postnummer"]
-    city_line = f"{zip_code} {city}"
-
-    parts = [street, floor_and_door, city_line]
-    line = ", ".join(p for p in parts if p)
-
-    return line
 
 
 class DatafordelerClient(httpx.Client):
@@ -68,7 +34,7 @@ class DatafordelerClient(httpx.Client):
         # initialize client with cert and default params
         super().__init__(
             cert=str(pem_file),
-            params={"format": "json", "pageSize": 200},
+            params={"format": "json"},
             verify=True,
             **kwargs,
         )
@@ -112,35 +78,6 @@ class DatafordelerClient(httpx.Client):
 
         return persons
 
-    def fech_citizens_data(self, data: dict) -> dict:
-
-        citizens = []
-        for person in data["Personer"]:
-            citizens.append(
-                {
-                    "cpr": next(person_info["Personnummer"]["personnummer"] for person_info in person["Person"]["Personnumre"] if person_info["Personnummer"]["status"] == "aktuel"),
-                    "name": next(f'{navn["Navn"]["fornavne"]} {navn["Navn"]["efternavn"]}' for navn in person["Person"]["Navne"] if navn["Navn"]["status"] == "aktuel"),
-                    "address": parse_address(
-                        address=next(address["Adresseoplysninger"]["CprAdresse"] for address in person["Person"]["Adresseoplysninger"] if address["Adresseoplysninger"]["CprAdresse"])
-                    ),
-                    "birthday": pd.to_datetime(str(person["Person"]["foedselsdato"])).to_pydatetime().date(),
-                    "civil_status": next((civil["Civilstand"]["Civilstandstype"] for civil in person["Person"]["Civilstande"] if civil["Civilstand"]["status"] == "aktuel"), None),
-                    "civil_valid_from": pd.to_datetime(
-                        next((civil["Civilstand"]["virkningFra"] for civil in person["Person"]["Civilstande"] if civil["Civilstand"]["status"] == "aktuel"), None)
-                    ).strftime("%d-%m-%Y"),
-                    "partner_cpr": next(
-                        (
-                            civil["Civilstand"]["Aegtefaelle"]["aegtefaellePersonnummer"]
-                            for civil in person["Person"]["Civilstande"]
-                            if (civil["Civilstand"]["status"] == "aktuel" and civil["Civilstand"]["Civilstandstype"] == "gift")
-                        ),
-                        None,
-                    ),
-                }
-            )
-
-        return citizens
-
 
 def prune_historical_records(obj: dict) -> dict:
     """
@@ -163,3 +100,36 @@ def prune_historical_records(obj: dict) -> dict:
             pruned[key] = value
 
     return pruned
+
+
+class DatafordelerAddress(TypedDict):
+    vejadresseringsnavn: str
+    husnummer: str
+    postnummer: str
+    postdistrikt: str
+    etage: NotRequired[str]
+    sidedoer: NotRequired[str]
+    bynavn: NotRequired[str]
+
+
+def parse_address(address: DatafordelerAddress) -> str:
+    """Parse a Datafordeler address into a single-line string on the form 'Street Name Nr, Floor Door, Zip City'."""
+
+    # https://danmarksadresser.dk/om-adresser/saadan-gengives-en-adresse
+
+    street_name = address["vejadresseringsnavn"]  # TODO: add supplerende bynavn
+    street_nr = re.sub(r"^0+", "", address["husnummer"])  # 005C, 063, 003A, etc.
+    street = f"{street_name} {street_nr}"
+
+    floor = re.sub(r"^0*(.+)$", r"\1.", address.get("etage", ""))  # "02" -> "2.", "st" -> "st."
+    door = address.get("sidedoer", "")  # "tv", "a19", etc.
+    floor_and_door = " ".join(p for p in (floor, door) if p)
+
+    city = address["postdistrikt"]  # Ebeltoft, Randers C, etc.
+    zip_code = address["postnummer"]
+    city_line = f"{zip_code} {city}"
+
+    parts = [street, floor_and_door, city_line]
+    line = ", ".join(p for p in parts if p)
+
+    return line
