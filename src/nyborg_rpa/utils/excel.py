fdb32d4a43f50b71b5c999ce7b0d2ce617187c2d
Martin Androvich <martinandrovich@gmail.com>
2025-09-19 17:15:32 +0200

    Add ignoring "number_stored_as_text" in Excel export

--- src/nyborg_rpa/utils/excel.py
+++ src/nyborg_rpa/utils/excel.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pandas as pd
+from xlsxwriter.utility import xl_range
 
 if TYPE_CHECKING:
     from xlsxwriter.worksheet import Worksheet
@@ -19,12 +20,15 @@ def df_to_excel_table(
     writer = pd.ExcelWriter(filepath, engine="xlsxwriter")
     df.to_excel(writer, sheet_name=sheet_name, startrow=1, header=False, index=False)
 
-    worksheet: Worksheet = writer.sheets[sheet_name]
+    ws: Worksheet = writer.sheets[sheet_name]
     (rows, cols) = df.shape
     column_settings = [{"header": column} for column in df.columns]
 
-    worksheet.add_table(0, 0, rows, cols - 1, {"columns": column_settings, "style": "Table Style Medium 2"})
-    worksheet.set_column(0, cols - 1, 1)
-    worksheet.autofit()
+    # silence "Number stored as text" over the data range
+    ws.ignore_errors({"number_stored_as_text": xl_range(1, 0, rows, cols - 1)})
+
+    ws.add_table(0, 0, rows, cols - 1, {"columns": column_settings, "style": "Table Style Medium 2"})
+    ws.set_column(0, cols - 1, 1)
+    ws.autofit()
 
     writer.close()
