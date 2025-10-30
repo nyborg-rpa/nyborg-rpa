b60bb4d56ddc3b3829f87108c48aa4819984fa9a
Emil Damholt Andersen <damholt-a@live.dk>
2025-10-30 10:50:41 +0100

    Add find_moved_patients_changes.py

---
+++ src/nyborg_rpa/scripts/nexus/find_moved_patienst_changes.py
@@ -0,0 +1,115 @@
+import os
+
+import argh
+from dotenv import load_dotenv
+
+from nyborg_rpa.utils.auth import get_user_login_info
+from nyborg_rpa.utils.email import send_email
+from nyborg_rpa.utils.nexus_client import NexusClient
+from nyborg_rpa.utils.pad import dispatch_pad_script
+
+nexus_client: NexusClient
+nexus_environment: str
+
+
+def find_moved_patients():
+    global nexus_client
+
+    # Find list "Borger fraflyttet kommunen med hjælpemiddel"
+    resp = nexus_client.get(url="preferences/CITIZEN_LIST")
+    resp.raise_for_status()
+    list_url = next(item for item in resp.json() if item["name"] == "Borger fraflyttet kommunen med hjælpemiddel")["_links"]["self"]["href"]
+
+    # Get patients in the list
+    resp = nexus_client.get(url=list_url)
+    resp.raise_for_status()
+    content_url = resp.json()["_links"]["content"]["href"]
+
+    resp = nexus_client.get(url=content_url, timeout=180)
+    resp.raise_for_status()
+    patients_view = resp.json()
+
+    moved_patients = set()
+    for page in patients_view["pages"]:
+        data: str = page["_links"]["patientData"]["href"]
+        moved_patients |= set(data.replace("https://nyborg.nexus.kmd.dk:443/api/core/mobile/nyborg/v2/patients?ids=", "").split(","))
+
+    return moved_patients
+
+
+def generate_report_email(moved_patients: set) -> str:
+
+    global nexus_environment
+
+    typography_style = "font-family: Arial, sans-serif; font-size: 12px"
+    body = f"""<!DOCTYPE html>
+    <html>
+    <body style="margin: 0; padding: 0; {typography_style};">
+    <p>Robotten har netop scannet "Borger fraflyttet kommunen med hjælpemiddel", og sammenlignet med sidste scanning. Den har nu identificeret følgende nye borgere på "Borger fraflyttet kommunen med hjælpemiddel":</p>
+    <table border="1" cellpadding="4" cellspacing="0" width="100%" style="border-collapse: collapse; {typography_style};">
+        <tr style="background-color: #dddddd; font-weight: bold;">
+        <td>Borgere</td>
+        </tr>"""
+
+    for patient_id in moved_patients:
+        body += f"""
+        <tr style="background-color: #f0f0f0; font-weight: bold;">
+        <td><a href="https://nyborg.{nexus_environment}.kmd.dk/citizen/{patient_id}" style="color: #0000EE;">{patient_id}</a></td>
+        </tr>"""
+
+    body += """
+    </table>
+    <p>Venlig hilsen,<br>Robotten</p>
+    </body>
+    </html>"""
+
+    return body
+
+
+@argh.arg("--recipients", help="List of email recipients for the report.", nargs="*")
+def find_moved_patients_changes(*, recipients: list[str]):
+    """Find moved patients and send email report if there are new ones."""
+
+    global nexus_client, nexus_environment
+
+    # Initialize Nexus client
+    login_info = get_user_login_info(
+        username="API",
+        program="Nexus-Drift",
+    )
+    nexus_environment = "nexus"
+    nexus_client = NexusClient(
+        client_id=login_info["username"],
+        client_secret=login_info["password"],
+        instance="nyborg",
+        enviroment=nexus_environment,
+    )
+
+    # Load previously moved patients
+    with open("src/nyborg_rpa/scripts/nexus/previous_moved_patients.txt", "r") as f:
+        previos_moved_patients = set([line.strip() for line in f])
+
+    # Find currently moved patients
+    moved_patients = find_moved_patients()
+    new_moved_patients = moved_patients - previos_moved_patients
+
+    if new_moved_patients:
+        print("New moved patients found")
+        load_dotenv(override=True)
+        send_email(
+            sender=os.environ["MS_MAILBOX"],
+            recipients=recipients,
+            subject="Rapport: Nye fraflyttede borgere med hjælpemiddel",
+            body=generate_report_email(new_moved_patients),
+        )
+    else:
+        print("No new moved patients")
+
+    # Save currently moved patients for next run
+    with open("src/nyborg_rpa/scripts/nexus/previous_moved_patients.txt", "w") as f:
+        for p in moved_patients:
+            f.write(p + "\n")
+
+
+if __name__ == "__main__":
+    dispatch_pad_script(fn=find_moved_patients_changes)
