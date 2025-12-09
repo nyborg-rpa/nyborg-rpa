c5a71c0f54ff17412abebe6e55f89fcd0aaa36fb
Martin Androvich <martinandrovich@gmail.com>
2025-12-09 16:54:10 +0100

    Fix TES sync using all rollekatalog users

--- src/nyborg_rpa/scripts/os2rollekatalog/tes_sync.py
+++ src/nyborg_rpa/scripts/os2rollekatalog/tes_sync.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from tqdm import tqdm
 
@@ -8,7 +9,7 @@ from nyborg_rpa.utils.pad import dispatch_pad_script
 from nyborg_rpa.utils.tunstall_client import TunstallGuiClient
 
 
-def tes_sync(user: str | None = None) -> list[str]:
+def tes_sync(*, user: str | None = None) -> list[str]:
     """Sync TES users with OS2rollekatalog role assignments."""
 
     user = user or os.environ["USERNAME"]
@@ -16,60 +17,67 @@ def tes_sync(user: str | None = None) -> list[str]:
     tes_client = TunstallGuiClient(user=user, password=password)
     rollekatalog_client = OS2rollekatalogClient(kommune="nyborg")
 
-    # Tunstall
+    # TODO: make TES client return ALL info about a user, e.g. search_users(..., detailed=True) which calls get_user(id)
     print("fetching TES users...")
     current_active_user = tes_client.search_user(role="Adgang til Borger", employee_text="Medarbejder")
     all_user = tes_client.search_user(role="Alle")
     all_active_user = tes_client.search_user(role="Adgang til Borger")
 
-    # OS2rollekatalog
     print("fetching TES - Medarbejder user from OS2rollekatalog...")
     roles_details = rollekatalog_client.get_userrole_details(role_name="TES - Medarbejder")
     roles_user_assignments = roles_details["assignments"]
 
-    # Compare lists and find users to add
+    print("fetching all users and their role assignments from OS2rollekatalog...")
+    users = {}
+    roles = rollekatalog_client.get("read/userroles").json()
+    for role in tqdm(roles):
+        resp = rollekatalog_client.get(f"read/assigned/{role["id"]}", params={"indirectRoles": "true"})
+        resp.raise_for_status()
+        assigned = resp.json()
+        for a in assigned.get("assignments", []):
+            u = users.setdefault(a["uuid"], {"uuid": a["uuid"], "userId": a.get("userId"), "name": a.get("name"), "roles": []})
+            u["roles"] += [role] if role not in u["roles"] else []
+
+    # compare lists and find users to add
     add_changes = []
+    for user in tqdm(roles_user_assignments, desc="Find add changes"):
 
-    for user in tqdm(roles_user_assignments, total=len(roles_user_assignments), desc="Find add changes"):
-        matches = next(
-            (
-                u
-                for u in current_active_user
-                if ((str(u["Brugernavn"]).lower() if "@" in str(u["Brugernavn"]) else f"{str(u['Brugernavn']).lower()}@nyborg.dk") == f"{user['userId'].lower()}@nyborg.dk")
-            ),
-            None,
-        )
-        if matches:
+        tes_user = next((u for u in all_user if re.sub(r"@.*", "", u["Brugernavn"]).lower() == user["userId"].lower()), None)
+        is_active = tes_user and tes_user["Brugernavn"] in [u["Brugernavn"] for u in all_active_user]
+
+        # user already exists and is active
+        if is_active:
             continue
-        else:
-            matches = next(
-                (u for u in all_user if ((str(u["Brugernavn"]).lower() if "@" in str(u["Brugernavn"]) else f"{str(u['Brugernavn']).lower()}@nyborg.dk") == f"{user['userId'].lower()}@nyborg.dk")), None
-            )
-            if matches:
-                check = next(
-                    (
-                        u
-                        for u in all_active_user
-                        if ((str(u["Brugernavn"]).lower() if "@" in str(u["Brugernavn"]) else f"{str(u['Brugernavn']).lower()}@nyborg.dk") == f"{user['userId'].lower()}@nyborg.dk")
-                    ),
-                    None,
-                )
-                if not check:
-                    add_changes.append(f"{matches['Navn']}, {user['userId']}, Tildel")
-            else:
-                add_changes.append(f"{user['name']}, {user['userId']}, Opret")
 
-    # Compare lists and find users to remove
+        # exists in TES but is not active
+        if tes_user:
+            add_changes += [f"{tes_user["Navn"]}, {user["userId"]}, Tildel"]
+
+        # does not exist in TES
+        else:
+            add_changes += [f"{user["name"]}, {user["userId"]}, Opret"]
+
+    # compare lists and find users to remove
     remove_changes = []
+    for user in tqdm(current_active_user, desc="Find remove changes"):
 
-    for user in tqdm(current_active_user, total=len(current_active_user), desc="Find remove changes"):
-        matches = next((u for u in roles_user_assignments if str(u["userId"]).lower() == f"{str(user["Brugernavn"]).replace("@nyborg.dk","")}".lower()), None)
-        if matches:
-            continue
-        else:
-            remove_changes.append(f"{user['Navn']}, {str(user["Brugernavn"]).replace("@nyborg.dk","")}, Fjern")
+        should_remove = False
+        tes_username = re.sub(r"@.*", "", user["Brugernavn"]).lower()
+        rollekatalog_user = next((u for u in users.values() if u["userId"].lower() == tes_username), {})
 
-    changes = add_changes + remove_changes
+        if not rollekatalog_user:
+            should_remove = True
+
+        systems = {r["itSystemName"] for r in rollekatalog_user.get("roles", [])}
+        if not any(s in systems for s in ["Tunstall TES", "Tunstall Service Provider"]):
+            should_remove = True
+
+        if should_remove:
+            remove_changes += [f"{user["Navn"]}, {tes_username}, Fjern"]
+
+    # NOTE: temporary disabled removal of users
+    changes = add_changes
+    # changes = add_changes + remove_changes
 
     return changes
 
