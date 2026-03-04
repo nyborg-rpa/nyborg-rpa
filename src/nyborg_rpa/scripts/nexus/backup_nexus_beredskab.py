0de7093038fd77245db0b97ce065b07b111d8448
Emil Damholt Andersen <damholt-a@live.dk>
2026-03-04 12:34:56 +0100

    Refactor backup_nexus_beredskab.py

--- src/nyborg_rpa/scripts/nexus/backup_nexus_beredskab.py
+++ src/nyborg_rpa/scripts/nexus/backup_nexus_beredskab.py
@@ -1,8 +1,10 @@
+import contextlib
 import os
 import shutil
 import time
 from datetime import datetime, timedelta
 from pathlib import Path
+from zoneinfo import ZoneInfo
 
 import argh
 import pandas as pd
@@ -17,69 +19,61 @@ from nyborg_rpa.utils.pad import dispatch_pad_script
 nexus_client: NexusClient
 
 
-def fetch_calendar_details(calendar_name: str) -> dict:
+def fetch_calendars() -> list[dict]:
     """Fetch calendar details for given calendar name."""
 
-    global nexus_client
-
     # Fetch calendar list and find calendar with given name
-    tqdm.write(f"Fetching calendar list to find calendar with name {calendar_name}")
     resp = nexus_client.get("/preferences/CROSS_CITIZEN_CALENDAR")
     resp.raise_for_status()
-    calendar_list = resp.json()
+    calendars = resp.json()
 
-    calendar = next((item for item in calendar_list if str(item["name"]).lower() == calendar_name.lower()), None)
-    assert calendar, f"Could not find calendar with name {calendar_name}"
-
-    tqdm.write(f"Fetching calendar data for {calendar_name}")
-    resp = nexus_client.get(calendar["_links"]["self"]["href"])
-    resp.raise_for_status()
-    calendar_data = resp.json()
-
-    # Fetch resource ids for calendar, all employess are resources
-    tqdm.write(f"Fetching resource ids for calendar {calendar_name}")
-    resp = nexus_client.get(calendar_data["_links"]["visits"]["href"], timeout=180)
-    resp.raise_for_status()
-    visits_data = resp.json()
-
-    # Get list of resource ids
-    resource_ids = [resource["resourceId"] for resource in visits_data["columnResource"]["resources"]]
-
-    # convert list of resource ids to comma separated string
-    resource_string = ",".join(resource_ids)
-    calendar_data["resourceIds"] = resource_string
-
-    return calendar_data
+    return calendars
 
 
-def fetch_koereliste(calendar_id: str, resource_ids: str, date: str, night: bool = False, save_path: Path = None) -> Path:
+def fetch_koereliste(*, calendar: dict, date: datetime, save_path: Path) -> Path:
     """Fetch calendar for given date and save as PDF file."""
 
-    global nexus_client
+    date = date.astimezone(ZoneInfo("UTC"))
 
-    # Convert date to ISO format with correct time
-    if night:
-        date_iso = pd.Timestamp(date, tz="UTC").replace(hour=23, minute=0, second=0, microsecond=0)
-    else:
-        date_iso = pd.Timestamp(date, tz="UTC").replace(hour=11, minute=0, second=0, microsecond=0)
+    # Fetch resource ids active on given date for calendar, resource ids are needed to fetch calendar pdf
+    tqdm.write(f"[{calendar['name'].upper()}] Fetching resource ids for calendar {calendar['name']}, date: {date}")
+    resp = nexus_client.post(
+        url=f"calendar/events/criteria/EVENT/{calendar['id']}",
+        params={
+            "calendarMode": "PLANNING_MODE",
+            "from": f"{date:%Y-%m-%dT%H:%M:%S.000Z}",
+            "to": f"{date:%Y-%m-%dT%H:%M:%S.000Z}",
+            "plannedGrantStatuses": "",
+            "registeredGrantStatuses": "",
+            "showPermanentResources": "false",
+            "showResourcesWithEvents": "true",
+            "showResourcesWithShifts": "false",
+        },
+        timeout=180,
+    )
+    resp.raise_for_status()
+    resource_data = resp.json()
 
-    date = date_iso.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-
-    # Create request body for calendar print list endpoint
-    body = {
-        "filterId": calendar_id,
-        "from": date,
-        "to": date,
-        "calendarMode": "PLANNING_MODE",
-        "resourceIds": resource_ids,
-        "plannedGrantStatuses": "",
-        "registeredGrantStatuses": "",
-        "zoomResource": None,
-    }
+    # Get list of resource ids, if resource is not visible, add - in front of id
+    resource_ids = [r["resourceId"] for r in resource_data["columnResource"]["resources"] if r["visible"]]
+    resource_string = ",".join(resource_ids)
 
     # Fetch calendar pdf and wait for result to be ready
-    tqdm.write(f"Requesting calendar PDF for {calendar_id} on {date}")
-    resp = nexus_client.post("calendar/printList/EVENT", json=body)
+    tqdm.write(f"[{calendar['name'].upper()}] Requesting calendar PDF for {calendar['name']} on {date}")
+
+    resp = nexus_client.post(
+        url="calendar/printList/EVENT",
+        json={
+            "filterId": calendar["id"],
+            "from": f"{date:%Y-%m-%dT%H:%M:%S.000Z}",
+            "to": f"{date:%Y-%m-%dT%H:%M:%S.000Z}",
+            "calendarMode": "PLANNING_MODE",
+            "resourceIds": resource_string,
+            "plannedGrantStatuses": "",
+            "registeredGrantStatuses": "",
+            "zoomResource": None,
+        },
+    )
     resp.raise_for_status()
     data = resp.json()
 
@@ -91,18 +85,15 @@ def fetch_koereliste(calendar_id: str, resource_ids: str, date: str, night: bool
         resp = nexus_client.get(data["_links"]["self"]["href"])
         data = resp.json()
     else:  # if we exit the loop without breaking, it means the result is not ready after 60 seconds
-        raise TimeoutError(f"Calendar PDF not ready after 60 seconds for {calendar_id} on {date}")
+        raise TimeoutError(f"Calendar PDF not ready after 60 seconds for {calendar['name']} on {date}")
+
+    # download pdf file
+    tqdm.write(f"[{calendar['name'].upper()}] Downloading calendar PDF for {calendar['name']} on {date}")
+    resp = nexus_client.get(data["_links"]["result"]["href"], timeout=300)
+    resp.raise_for_status()
 
     # save to pdf file
-    if save_path is None:
-        save_path = Path("~/Downloads").expanduser().resolve() / f"{calendar_id}.pdf"
-
-    with nexus_client.stream("GET", data["_links"]["result"]["href"], timeout=180) as resp:
-        total = int(resp.headers.get("content-length", 0))
-        with open(save_path, "wb") as f, tqdm(total=total, unit="B", unit_scale=True) as pbar:
-            for chunk in resp.iter_bytes():
-                f.write(chunk)
-                pbar.update(len(chunk))
+    save_path.write_bytes(resp.content)
 
     return save_path
 
@@ -127,40 +118,48 @@ def backup_nexus_beredskab(*, recipients: list[str]):
     )
 
     # load emergency drive path from env variable and read csv file with calendar names and types
-    emergency_drive_path = Path(os.environ["NEXUS_EMERGENCY_DRIVE"])
-
-    csv_file = Path(os.environ["NEXUS_EMERGENCY_CSV"])
+    emergency_path = Path(os.environ["NEXUS_EMERGENCY_DRIVE"])
+    emergency_calendar_path = emergency_path / "Kørelister"
+    csv_file = emergency_path / "Setting/Kørelister.csv"
     df = pd.read_csv(csv_file, encoding="utf-8-sig", sep=None, engine="python")
 
     # Get current date and create folder for today's date in each calendar folder
-    now = pd.Timestamp.now()
+    now = datetime.now(tz=ZoneInfo("Europe/Copenhagen"))
 
-    error_messages = ""
+    # Fetch all calendars
+    calendars = fetch_calendars()
+
     # Loop through each row in csv file and fetch calendar for given date and next 2 days, save as PDF file in corresponding folder in emergency drive
+    error_messages = ""
     for i, row in tqdm(df.iterrows(), total=len(df), desc="Fetching calendars"):
 
         koereliste = row["Køreliste"]
         night = str(row["Type"]).strip().lower() == "nat"
 
-        koereliste_path = emergency_drive_path / f"{now:%Y-%m-%d}/{koereliste}"
+        koereliste_path = emergency_calendar_path / f"{now:%Y-%m-%d}/{koereliste}"
         koereliste_path.mkdir(parents=True, exist_ok=True)
 
         try:
-            tqdm.write(f"Fetching {koereliste} (Night: {night})")
-            calendar_details = fetch_calendar_details(koereliste)
-            dates = [now + timedelta(days=i) for i in range(-3, 4)]
+            tqdm.write(f"Fetching {koereliste} ({night=})...")
+
+            calendar = next((item for item in calendars if str(item["name"]).lower() == koereliste.lower()), None)
+            assert calendar, f"Could not find calendar with name {koereliste}"
+
+            dates = []
+            for i in range(-3, 4):
+                hour = 12 if night else 00
+                dates += [(now + timedelta(days=i)).replace(hour=hour, minute=0, second=0, microsecond=0)]
 
             for date in dates:
-
                 destination_file = koereliste_path / f"{koereliste}_{date:%Y-%m-%d}.pdf"
                 if destination_file.exists():
                     continue
 
-                fetch_koereliste(calendar_id=calendar_details["id"], resource_ids=calendar_details["resourceIds"], date=date, night=night, save_path=destination_file)
-                tqdm.write(f"{destination_file}")
+                fetch_koereliste(calendar=calendar, date=date, save_path=destination_file)
+                tqdm.write(f"[{koereliste.upper()}] Saved {destination_file}")
 
         except Exception as e:
-            tqdm.write(f"Error fetching calendar for {koereliste} on {date}: {e}")
+            tqdm.write(f"[{koereliste.upper()}] Error fetching calendar for {koereliste} on {date}: {e}")
             error_messages += f"<p>Error fetching calendar for {koereliste} on {date}: {e}</p>"
 
     # Send email if there were any errors
@@ -182,14 +181,12 @@ def backup_nexus_beredskab(*, recipients: list[str]):
 
     # get all folder in emergency drive path except for folders that are newer than 3 days and delete them, shall also handle case where folder name is not a date
     folders = []
-    for f in emergency_drive_path.iterdir():
+    for f in emergency_calendar_path.iterdir():
         if f.is_dir():
-            try:
+            with contextlib.suppress(Exception):
                 folder_date = datetime.strptime(f.name, "%Y-%m-%d")
                 if folder_date < (now - timedelta(days=3)):
                     folders.append(f)
-            except:
-                continue
 
     for folder in folders:
         print(f"Processing {folder.name}")
